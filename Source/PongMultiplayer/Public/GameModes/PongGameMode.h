797918df8a54df7e444152c6f57d5ce7667e5994
Ivan Seleznov <ivan14.02434@gmail.com>
2023-10-06 13:02:21 +0300

    Initial commit

---
+++ Source/PongMultiplayer/Public/GameModes/PongGameMode.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "PongGameMode.generated.h"
+
+class ABaseCharacter;
+/**
+ * 
+ */
+UCLASS()
+class PONGMULTIPLAYER_API APongGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+};
